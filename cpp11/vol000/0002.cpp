7654ab743a3f2fe256094c7bd8275e0c990cbb3d
aoisensi <aoisensi@aoisensi.info>
2014-04-10 16:36:26 +0900

    w

--- cpp11/vol000/0002.cpp
+++ cpp11/vol000/0002.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-	int a, b;
-	while(cin >> a >> b)
-		cout << (a + b) << endl;
-	return 0;
+    int a, b;
+    while(cin >> a >> b)
+        cout << (1 + (int)log10(a + b)) << endl;
+    return 0;
 }
